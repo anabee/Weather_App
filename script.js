9e616ab2fe70f6180ed60e1fbe9ebac7eb0fc01d
Ana Valdivia <anav1317@yahoo.com>
2020-02-03 21:23:37 -0800

    Added UV Index

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ function searchWeather(city){
         method: "GET"
       }).then(function(response){
 
+        // CITY NAME 
         var cityNameDiv = $("<div>")
         $(".citySpecifics").append(cityNameDiv)
         cityNameDiv.text(response.name +" "+ moment().format("MMM Do YY"))
 
+        // WEATHER ICON
         var weatherImg = $("<img>")
         $(".citySpecifics").append(weatherImg)
         var iconID = (JSON.stringify(response.weather[0].icon))
@@ -19,6 +21,7 @@ function searchWeather(city){
         var imgURL = "http://openweathermap.org/img/wn/"+iconIdSubS+"@2x.png"
         $(weatherImg).attr("src", imgURL)
 
+        // TEMPERATURE AND CONVERSION FORMULA
         var temperature = $("<div>")
         $(".citySpecifics").append(temperature)
 
@@ -29,15 +32,49 @@ function searchWeather(city){
           }
           changeTemp()
 
+        // HUMIDITY 
         var humidity = $("<div>")
         $(".citySpecifics").append(humidity)
         humidity.text("Humidity: " +response.main.humidity+"%")
 
+        // WIND SPEED 
         var windSpeed = $("<div>")
         $(".citySpecifics").append(windSpeed)
         windSpeed.text("Wind Speed: " +response.wind.speed+" MPH")
 
-        // need UV Index
+        //UV INDEX
+        var latOut = response.coord.lat
+        var lonOut = response.coord.lon
+        function getUV(lat,lon){
+           var lat = latOut
+           var lon = lonOut
+
+            var queryURLuv = "http://api.openweathermap.org/data/2.5/uvi?appid=aa5b71088d417c59bb0ed00cd6cfe06e&lat="+lat+"&lon="+lon
+
+            $.ajax({
+                url: queryURLuv,
+                method: "GET"
+              }).then(function(response){
+
+                var uvInd = $("<div>")
+                $(".citySpecifics").append(uvInd)
+                uvInd.text("UV Index: "+response.value)
+
+                if (response.value <=2){
+                    uvInd.css({"background-color": "green"})
+                } else if ( response.value <=5){
+                    uvInd.css({"background-color": "yellow"})
+                } else if (response.value <=7){
+                    uvInd.css({"background-color": "orange"})
+                } else if (response.value <=10){
+                    uvInd.css({"background-color": "red"})
+                } else if (response.value >= 11){
+                    uvInd.css({"background-color": "purple"})
+                }
+              })
+        }
+        getUV()
+
         })
 }
 
